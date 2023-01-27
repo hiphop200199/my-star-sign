26928d4979ca2f8f752d5b6819309cdf3fd5f7cf
Eric Tsai <hiphop200199@gmail.com>
2023-01-27 09:14:39 +0800

    增加圖片及文章

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,113 @@
 <script>
+  import "./style.scss";
+  import capricorn from "./assets/capricorn.png";
+  import scorpio from "./assets/scorpio.png";
+  import libra from "./assets/libra.png";
+  import sagittarius from "./assets/sagittarius.png";
+  import aquarius from "./assets/aquarius.png";
+  import pisces from "./assets/pisces.png";
+  import aries from "./assets/aries.png";
+  import virgo from "./assets/virgo.png";
+  import taurus from "./assets/taurus.png";
+  import cancer from "./assets/cancer.png";
+  import gemini from "./assets/gemini.png";
+  import leo from "./assets/leo.png";
 </script>
 
 <main>
-  <h1>Vite + Svelte</h1>
+  <button>What`s your star sign?</button>
+  <div class="star-sign-card">
+    <img src={capricorn} alt="capricorn" />
+    <h4>摩羯座</h4>
+    <h6>12/22~1/20</h6>
+    <p>
+      非常自律 ，為達目的會堅持不懈 ，謹慎、可靠
+      、有責任感，對自己有相當的抱負。不熟的人會覺得有一層距離感，無法真的了解他們的內心想法，也因此給人一種什麼事都自己來的感覺，然而只要遇到值得信賴的人，便會坦誠相待。
+    </p>
+  </div>
+  <div class="star-sign-card">
+    <img src={aquarius} alt="aquarius" />
+    <h4>水瓶座</h4>
+    <h6>1/21~2/18</h6>
+    <p>
+      求知慾強、興趣廣泛，樂於嘗試不同的新事物，且創意十足
+      ，富有改革與冒險家精神 。 也是大家一致公認的外星人星座，個性古怪
+      ，很常會陷入自己的小世界，對許多事有獨樹一格的見解，有些人認為水瓶座在工作上很叛逆
+      、不合群，且容易心不在焉 。
+    </p>
+  </div>
+  <div class="star-sign-card">
+    <img src={pisces} alt="pisces" />
+    <h4>雙魚座</h4>
+    <h6>2/19~3/20</h6>
+    <p>
+      感情豐沛、極富同情心又善解人意，是個標準的浪漫主義者
+      ，天性溫柔無私且很會察言觀色，然而因為有時太憑感覺做事，想法單純
+      ，反而給人不切實際 、軟弱、逃避現實的印象。
+    </p>
+  </div>
+
+  <div class="star-sign-card">
+    <img src={aries} alt="aries" />
+    <h4>牡羊座</h4>
+    <h6>3/21~4/20</h6>
+    <p>
+      生性大方、開朗
+      ，待人親切，即便是初次見面的陌生人，都能很快地打成一片，且行事風格自由，不喜歡受拘束，具冒險精神
+      。然而，有時因過度急躁
+      ，且無法接受批評，反而會弄巧成拙，建議適時將腳步放慢，多點耐心，在工作或人際相處上都會更順利喔！
+    </p>
+  </div>
+  <div class="star-sign-card">
+    <img src={taurus} alt="taurus" />
+    <h4>金牛座</h4>
+    <h6>4/21~5/21</h6>
+    <p>
+      個性勤勞 、務實
+      、穩重，且溫和順從，總是笑臉待人且不管別人閒事，是高明的社交高手，他們很有主見、有耐心及責任感
+      ，且具有商業頭腦。但也是出了名的固執，且因他們理性的思考模式，偶爾給人過於冷淡的印象。
+    </p>
+  </div>
+  <div class="star-sign-card">
+    <img src={gemini} alt="gemini" />
+    <h4>雙子座</h4>
+    <h6>5/22~6/21</h6>
+    <p />
+  </div>
+  <div class="star-sign-card">
+    <img src={cancer} alt="cancer" />
+    <h4>巨蟹座</h4>
+    <h6>6/22~7/22</h6>
+    <p />
+  </div>
+  <div class="star-sign-card">
+    <img src={leo} alt="leo" />
+    <h4>獅子座</h4>
+    <h6>7/23~8/22</h6>
+    <p />
+  </div>
+  <div class="star-sign-card">
+    <img src={virgo} alt="virgo" />
+    <h4>處女座</h4>
+    <h6>8/23~9/22</h6>
+    <p />
+  </div>
+  <div class="star-sign-card">
+    <img src={libra} alt="libra" />
+    <h4>天秤座</h4>
+    <h6>9/23~10/23</h6>
+    <p />
+  </div>
+  <div class="star-sign-card">
+    <img src={scorpio} alt="scorpio" />
+    <h4>天蠍座</h4>
+    <h6>10/24~11/22</h6>
+    <p />
+  </div>
+  <div class="star-sign-card">
+    <img src={sagittarius} alt="sagittarius" />
+    <h4>射手座</h4>
+    <h6>11/23~12/21</h6>
+    <p />
+  </div>
 </main>
